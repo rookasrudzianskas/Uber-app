9bcc79756de71b07fab477809513c3d7a03bbb07
Rokas <rookas.rudzenskas@gmail.com>
2021-07-31 08:32:17 +0300

    Rokas

--- src/components/UberTypeRow/index.js
+++ src/components/UberTypeRow/index.js
@@ -19,7 +19,7 @@ const UberTypeRow = (props) => {
     }
 
     return (
-        <Pressable onPress={onPress} style={[styles.container, {backgroundColor: isSelected ? "#efefef" : 'white'}]}>
+        <Pressable onPress={onPress} style={[styles.container, {backgroundColor: isSelected ? "white" : '#efefef'}]}>
 
             <Image style={styles.image} source={getImageName()} />
             <View style={styles.middleContainer}>
