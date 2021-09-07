ccf4e2811cee988ad96952e674c5667d828117f7
Rokas <rookas.rudzenskas@gmail.com>
2021-09-07 11:42:37 +0300

    Rokas

--- src/screens/LoginScreen/index.js
+++ src/screens/LoginScreen/index.js
@@ -10,50 +10,60 @@ const LoginScreen = () => {
         const text = 'Hello, my container is blurring contents underneath!';
 
         return (
-            <View style={styles.container}>
-                <Image style={[StyleSheet.absoluteFill, styles.image]} source={{ uri }} />
-                <BlurView intensity={90} tint="dark" style={styles.blurContainer}>
-                    <View style={tw`flex `}>
-                    <View style={tw`flex mb-36 flex-row items-center justify-center`}>
-                        <FontAwesome5 name="uber" size={40} color="white" />
-                        <Text style={tw`text-xl text-white ml-4`}>
-                            Login to Uber
-                        </Text>
+            // <View style={styles.container}>
+            //     <Image style={[StyleSheet.absoluteFill, styles.image]} source={{ uri }} />
+            //     <BlurView intensity={90} tint="dark" style={styles.blurContainer}>
+            //         <View style={tw`flex `}>
+            //         <View style={tw`flex mb-36 flex-row items-center justify-center`}>
+            //             <FontAwesome5 name="uber" size={40} color="white" />
+            //             <Text style={tw`text-xl text-white ml-4`}>
+            //                 Login to Uber
+            //             </Text>
+            //
+            //         </View>
+            //
+            //
+            //         <TouchableOpacity activeOpacity={0.8}>
+            //             <View style={tw`flex flex-row items-center justify-center  bg-gray-800 py-3 px-3 rounded-full`}>
+            //                 <AntDesign name="google" size={25} color="white" />
+            //                 <Text style={tw`text-xl text-white ml-4 font-bold ml-2`}>Sign in with Google</Text>
+            //             </View>
+            //         </TouchableOpacity>
+            //         </View>
+            //     </BlurView>
+            // </View>
+            <View style={tw`items-center flex-1 bg-blue-700`}>
+             {/*    <Image style={[StyleSheet.absoluteFill, styles.image]} source={{ uri }} />*/}
+             {/*       <BlurView intensity={90} tint="dark" style={styles.blurContainer}>*/}
+             {/*       <View style={tw`flex items-center justify-center flex-1 -mt-32`}>*/}
+             {/*           <Text style={tw` text-3xl font-bold mt-24 text-gray-200 text-center`}>Proudly powered by <Text style={tw`text-green-500 font-extrabold`}>Rokas Rudzianskas</Text> and made with ❤️</Text>*/}
+             {/*       </View>*/}
 
-                    </View>
 
+             {/*       /!*            <View style={tw`absolute bottom-0 left-0`}>*!/*/}
+             {/*       <View style={tw``}>*/}
+             {/*           <TouchableOpacity activeOpacity={0.8} >*/}
+             {/*               <View style={tw`px-16 py-5 bg-white flex flex-row items-center border-4 border-blue-500 rounded-xl  mt-2`}>*/}
+             {/*                   <AntDesign style={tw`mr-5`} name="google" size={24} color="black" />*/}
+             {/*                   <Text style={tw`text-gray-700 text-center text-lg font-bold`}>Sign in with Google!</Text>*/}
+             {/*               </View>*/}
+             {/*           </TouchableOpacity>*/}
 
-                    <TouchableOpacity activeOpacity={0.8}>
-                        <View style={tw`flex flex-row items-center justify-center  bg-gray-800 py-3 px-3 rounded-full`}>
-                            <AntDesign name="google" size={25} color="white" />
-                            <Text style={tw`text-xl text-white ml-4 font-bold ml-2`}>Sign in with Google</Text>
-                        </View>
-                    </TouchableOpacity>
-                    </View>
-                </BlurView>
+             {/*           <TouchableOpacity activeOpacity={0.8} >*/}
+             {/*               <View style={tw`px-16 py-5 bg-white flex flex-row items-center border-4 border-blue-500 rounded-xl  mt-2`}>*/}
+             {/*                   <AntDesign style={tw`mr-5`}  name="apple1" size={24} color="black" />*/}
+             {/*                   <Text style={tw`text-gray-700 text-center text-lg font-bold`}>Sign in with Apple!</Text>*/}
+             {/*               </View>*/}
+             {/*           </TouchableOpacity>*/}
+             {/*       </View>*/}
+
+             {/*</View>*/}
+             {/* </BlurView>*/}
             </View>
         );
     }
 
-    const styles = StyleSheet.create({
-        container: {
-            flex: 1
-        },
-        image: {
-            width: '100%',
-            height: '100%',
-            resizeMode: 'cover',
-        },
-        blurContainer: {
-            flex: 1,
-            padding: 40,
-            justifyContent: 'center',
-        },
-        text: {
-            fontSize: 24,
-            fontWeight: '600'
-        },
-    });
+
 
     export default LoginScreen;
 
