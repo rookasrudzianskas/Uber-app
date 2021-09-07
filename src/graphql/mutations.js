5740a4ae2fd656c8429637f333774640534a2914
Rokas <rookas.rudzenskas@gmail.com>
2021-09-07 14:07:22 +0300

    Rokas

---
+++ src/graphql/mutations.js
@@ -0,0 +1,306 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createUser = /* GraphQL */ `
+  mutation CreateUser(
+    $input: CreateUserInput!
+    $condition: ModelUserConditionInput
+  ) {
+    createUser(input: $input, condition: $condition) {
+      id
+      username
+      email
+      orders {
+        items {
+          id
+          createdAt
+          userId
+          type
+          originLatitude
+          originLongitude
+          destLatitude
+          destLongitude
+          carId
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateUser = /* GraphQL */ `
+  mutation UpdateUser(
+    $input: UpdateUserInput!
+    $condition: ModelUserConditionInput
+  ) {
+    updateUser(input: $input, condition: $condition) {
+      id
+      username
+      email
+      orders {
+        items {
+          id
+          createdAt
+          userId
+          type
+          originLatitude
+          originLongitude
+          destLatitude
+          destLongitude
+          carId
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteUser = /* GraphQL */ `
+  mutation DeleteUser(
+    $input: DeleteUserInput!
+    $condition: ModelUserConditionInput
+  ) {
+    deleteUser(input: $input, condition: $condition) {
+      id
+      username
+      email
+      orders {
+        items {
+          id
+          createdAt
+          userId
+          type
+          originLatitude
+          originLongitude
+          destLatitude
+          destLongitude
+          carId
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const createCar = /* GraphQL */ `
+  mutation CreateCar(
+    $input: CreateCarInput!
+    $condition: ModelCarConditionInput
+  ) {
+    createCar(input: $input, condition: $condition) {
+      id
+      type
+      latitude
+      longitude
+      heading
+      orders {
+        items {
+          id
+          createdAt
+          userId
+          type
+          originLatitude
+          originLongitude
+          destLatitude
+          destLongitude
+          carId
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateCar = /* GraphQL */ `
+  mutation UpdateCar(
+    $input: UpdateCarInput!
+    $condition: ModelCarConditionInput
+  ) {
+    updateCar(input: $input, condition: $condition) {
+      id
+      type
+      latitude
+      longitude
+      heading
+      orders {
+        items {
+          id
+          createdAt
+          userId
+          type
+          originLatitude
+          originLongitude
+          destLatitude
+          destLongitude
+          carId
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteCar = /* GraphQL */ `
+  mutation DeleteCar(
+    $input: DeleteCarInput!
+    $condition: ModelCarConditionInput
+  ) {
+    deleteCar(input: $input, condition: $condition) {
+      id
+      type
+      latitude
+      longitude
+      heading
+      orders {
+        items {
+          id
+          createdAt
+          userId
+          type
+          originLatitude
+          originLongitude
+          destLatitude
+          destLongitude
+          carId
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const createOrder = /* GraphQL */ `
+  mutation CreateOrder(
+    $input: CreateOrderInput!
+    $condition: ModelOrderConditionInput
+  ) {
+    createOrder(input: $input, condition: $condition) {
+      id
+      createdAt
+      userId
+      user {
+        id
+        username
+        email
+        orders {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      type
+      originLatitude
+      originLongitude
+      destLatitude
+      destLongitude
+      carId
+      car {
+        id
+        type
+        latitude
+        longitude
+        heading
+        orders {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      updatedAt
+    }
+  }
+`;
+export const updateOrder = /* GraphQL */ `
+  mutation UpdateOrder(
+    $input: UpdateOrderInput!
+    $condition: ModelOrderConditionInput
+  ) {
+    updateOrder(input: $input, condition: $condition) {
+      id
+      createdAt
+      userId
+      user {
+        id
+        username
+        email
+        orders {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      type
+      originLatitude
+      originLongitude
+      destLatitude
+      destLongitude
+      carId
+      car {
+        id
+        type
+        latitude
+        longitude
+        heading
+        orders {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      updatedAt
+    }
+  }
+`;
+export const deleteOrder = /* GraphQL */ `
+  mutation DeleteOrder(
+    $input: DeleteOrderInput!
+    $condition: ModelOrderConditionInput
+  ) {
+    deleteOrder(input: $input, condition: $condition) {
+      id
+      createdAt
+      userId
+      user {
+        id
+        username
+        email
+        orders {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      type
+      originLatitude
+      originLongitude
+      destLatitude
+      destLongitude
+      carId
+      car {
+        id
+        type
+        latitude
+        longitude
+        heading
+        orders {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      updatedAt
+    }
+  }
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,279 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateUser = /* GraphQL */ `
+  subscription OnCreateUser {
+    onCreateUser {
+      id
+      username
+      email
+      orders {
+        items {
+          id
+          createdAt
+          userId
+          type
+          originLatitude
+          originLongitude
+          destLatitude
+          destLongitude
+          carId
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateUser = /* GraphQL */ `
+  subscription OnUpdateUser {
+    onUpdateUser {
+      id
+      username
+      email
+      orders {
+        items {
+          id
+          createdAt
+          userId
+          type
+          originLatitude
+          originLongitude
+          destLatitude
+          destLongitude
+          carId
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteUser = /* GraphQL */ `
+  subscription OnDeleteUser {
+    onDeleteUser {
+      id
+      username
+      email
+      orders {
+        items {
+          id
+          createdAt
+          userId
+          type
+          originLatitude
+          originLongitude
+          destLatitude
+          destLongitude
+          carId
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onCreateCar = /* GraphQL */ `
+  subscription OnCreateCar {
+    onCreateCar {
+      id
+      type
+      latitude
+      longitude
+      heading
+      orders {
+        items {
+          id
+          createdAt
+          userId
+          type
+          originLatitude
+          originLongitude
+          destLatitude
+          destLongitude
+          carId
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateCar = /* GraphQL */ `
+  subscription OnUpdateCar {
+    onUpdateCar {
+      id
+      type
+      latitude
+      longitude
+      heading
+      orders {
+        items {
+          id
+          createdAt
+          userId
+          type
+          originLatitude
+          originLongitude
+          destLatitude
+          destLongitude
+          carId
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteCar = /* GraphQL */ `
+  subscription OnDeleteCar {
+    onDeleteCar {
+      id
+      type
+      latitude
+      longitude
+      heading
+      orders {
+        items {
+          id
+          createdAt
+          userId
+          type
+          originLatitude
+          originLongitude
+          destLatitude
+          destLongitude
+          carId
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onCreateOrder = /* GraphQL */ `
+  subscription OnCreateOrder {
+    onCreateOrder {
+      id
+      createdAt
+      userId
+      user {
+        id
+        username
+        email
+        orders {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      type
+      originLatitude
+      originLongitude
+      destLatitude
+      destLongitude
+      carId
+      car {
+        id
+        type
+        latitude
+        longitude
+        heading
+        orders {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      updatedAt
+    }
+  }
+`;
+export const onUpdateOrder = /* GraphQL */ `
+  subscription OnUpdateOrder {
+    onUpdateOrder {
+      id
+      createdAt
+      userId
+      user {
+        id
+        username
+        email
+        orders {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      type
+      originLatitude
+      originLongitude
+      destLatitude
+      destLongitude
+      carId
+      car {
+        id
+        type
+        latitude
+        longitude
+        heading
+        orders {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      updatedAt
+    }
+  }
+`;
+export const onDeleteOrder = /* GraphQL */ `
+  subscription OnDeleteOrder {
+    onDeleteOrder {
+      id
+      createdAt
+      userId
+      user {
+        id
+        username
+        email
+        orders {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      type
+      originLatitude
+      originLongitude
+      destLatitude
+      destLongitude
+      carId
+      car {
+        id
+        type
+        latitude
+        longitude
+        heading
+        orders {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      updatedAt
+    }
+  }
+`;
