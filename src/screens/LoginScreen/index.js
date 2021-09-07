3af454e55e5b8c1557834ae21b68199d03e33ccc
Rokas <rookas.rudzenskas@gmail.com>
2021-09-07 10:09:33 +0300

    Rokas

--- src/screens/LoginScreen/index.js
+++ src/screens/LoginScreen/index.js
@@ -1,13 +1,42 @@
 import React from 'react';
-import {Text, View} from "react-native";
 import tw from 'tailwind-react-native-classnames';
+import { Image, Text, StyleSheet, View } from 'react-native';
+import { BlurView } from 'expo-blur';
 
 const LoginScreen = () => {
-    return (
-        <View>
-            <Text>Rokas</Text>
-        </View>
-    );
-};
 
-export default LoginScreen;
+    const uri = 'https://images.unsplash.com/photo-1594058573823-d8edf1ad3380?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=998&q=80';
+        const text = 'Hello, my container is blurring contents underneath!';
+
+        return (
+            <View style={styles.container}>
+                <Image style={[StyleSheet.absoluteFill, styles.image]} source={{ uri }} />
+                <BlurView intensity={90} tint="dark" style={styles.blurContainer}>
+
+                </BlurView>
+            </View>
+        );
+    }
+
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1
+        },
+        image: {
+            width: '100%',
+            height: '100%',
+            resizeMode: 'cover',
+        },
+        blurContainer: {
+            flex: 1,
+            padding: 20,
+            justifyContent: 'center',
+        },
+        text: {
+            fontSize: 24,
+            fontWeight: '600'
+        },
+    });
+
+    export default LoginScreen;
+
