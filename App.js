5280f48028bb322fb6e0ded3abedbeb7ef2c0af6
Rokas <rookas.rudzenskas@gmail.com>
2021-09-07 14:14:42 +0300

    Rokas

--- App.js
+++ App.js
@@ -4,12 +4,19 @@ import {Platform, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import * as Location from "expo-location";
 import RouteNavigator from "./src/navigation/router";
 import {NavigationContainer} from "@react-navigation/native";
-import Amplify from 'aws-amplify';
+import Amplify, {Auth} from 'aws-amplify';
 import config from './src/aws-exports';
 
 
 import { withAuthenticator } from 'aws-amplify-react-native';
 
+Amplify.configure({
+    Auth: {
+        region: 'us-east-1',
+        userPoolId: 'us-east-1_RazNdoT8l',
+        userPoolWebClientId: '1709if2u7o0g9fi55mttiid14k',
+    }
+});
 
 
 function App() {
