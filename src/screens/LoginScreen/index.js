97334658b068b38d669df49709543d18b05edeeb
Rokas <rookas.rudzenskas@gmail.com>
2021-09-07 12:25:26 +0300

    Rokas

--- src/screens/LoginScreen/index.js
+++ src/screens/LoginScreen/index.js
@@ -6,35 +6,12 @@ import {AntDesign, FontAwesome5} from '@expo/vector-icons';
 
 const LoginScreen = () => {
 
-    const uri = 'https://images.unsplash.com/photo-1594058573823-d8edf1ad3380?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=998&q=80';
-        const text = 'Hello, my container is blurring contents underneath!';
+    const uri = 'https://images.unsplash.com/photo-1512850183-6d7990f42385?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80';
 
         return (
-            // <View style={styles.container}>
-            //     <Image style={[StyleSheet.absoluteFill, styles.image]} source={{ uri }} />
-            //     <BlurView intensity={90} tint="dark" style={styles.blurContainer}>
-            //         <View style={tw`flex `}>
-            //         <View style={tw`flex mb-36 flex-row items-center justify-center`}>
-            //             <FontAwesome5 name="uber" size={40} color="white" />
-            //             <Text style={tw`text-xl text-white ml-4`}>
-            //                 Login to Uber
-            //             </Text>
-            //
-            //         </View>
-            //
-            //
-            //         <TouchableOpacity activeOpacity={0.8}>
-            //             <View style={tw`flex flex-row items-center justify-center  bg-gray-800 py-3 px-3 rounded-full`}>
-            //                 <AntDesign name="google" size={25} color="white" />
-            //                 <Text style={tw`text-xl text-white ml-4 font-bold ml-2`}>Sign in with Google</Text>
-            //             </View>
-            //         </TouchableOpacity>
-            //         </View>
-            //     </BlurView>
-            // </View>
             <View style={tw`items-center flex-1 bg-blue-700`}>
                  <Image style={[StyleSheet.absoluteFill, styles.image]} source={{ uri }} />
-                        <BlurView intensity={90} tint="dark" style={styles.blurContainer}>
+                        <BlurView intensity={60} tint="dark" style={styles.blurContainer}>
 
                     <View style={tw`flex flex-row items-center justify-center flex-1 -mt-32`}>
                         <FontAwesome5 name="uber" size={40} color="white" />
@@ -44,7 +21,7 @@ const LoginScreen = () => {
                     </View>
 
                         <View style={tw`flex items-center justify-center`}>
-                    <View style={tw`absolute bottom-0 left-15 mb-24`}>
+                    <View style={tw`absolute bottom-0 mb-24`}>
                             <TouchableOpacity activeOpacity={0.8} >
                                 <View style={tw`px-11 py-3 bg-black flex flex-row items-center border-2 border-gray-300 rounded-full  mt-2`}>
                                     <FontAwesome5 name="uber" size={25} color="white" />
